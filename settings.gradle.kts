6f484458b99f1c8eb69ee4656e832b1d74c405cb
gauravguptathinksys <gupta.gaurav1@thinksys.com>
2022-07-08 15:24:48 +0530

    added code half

---
+++ settings.gradle.kts
@@ -0,0 +1,11 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "PyTorchDemo"
+include(":androidApp")
+include(":shared")
